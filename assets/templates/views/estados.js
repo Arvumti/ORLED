4c980c6c9cfbf74b8102b9c8de21bd706d40bcae
Vanessa Del Angel <vane_5933@hotmail.com>
2015-09-16 15:13:01 -0500

    Cambios Vane

---
+++ assets/templates/views/estados.js
@@ -0,0 +1,44 @@
+var MoEstados = Backbone.Model.extend({
+	defaults: {
+		idEstado	:0,
+		nombre		:'',
+		idPais		:0,
+	}
+});
+define(['/js/base/viewsBase.js'], function (viewsBase) {
+	/*
+		columns: columnas del grid
+		model: modelo [opcional]
+		pk: primary key
+		url: ruta del api
+	*/
+	var ViEstados = viewsBase.abc.extend({
+		el: '#estados',
+		events: {
+			'click .btn-aceptar' :'click_aceptar'
+		},
+		initialize: function() {
+			this.pk = 'idEstado';
+			this.url = '/estados';
+			this.model = MoEstados;
+
+			this.fks = {
+				idPais: {
+					url: 'paises',
+					filters: [{filter:'nombre'}],
+				},
+			};
+
+			this.extras = {
+				clean: ['nombre','idPais'],
+			};
+
+			var columns = [
+				{nombre:'Estado', field:'nombre', width:400},
+				{nombre:'País', field:'idPais.nombre', width:200},
+			];
+			viewsBase.abc.prototype.initialize.call(this, columns, null);
+		},
+	});
+	return {view: ViEstados};
+});
