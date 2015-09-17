b7d302aecb2c9b061a71961a9c8719e7ab3c633c
eumati <eloy.urbina@arvumti.com>
2015-09-17 18:47:58 -0500

    configuracion

--- api/controllers/IrradianciasDiasController.js
+++ api/controllers/IrradianciasDiasController.js
@@ -6,6 +6,17 @@
  */
 
 module.exports = {
-	
+	PromedioDia: function (req, res, next) {
+		var idLocalidad = req.param('idLocalidad') || 0;
+		var query = ' \
+			SELECT 	ROUND((enero+febrero+marzo+abril+mayo+junio+julio+agosto+septiembre+octubre+noviembre+diciembre)/12, 3) promedio,  \
+					hora \
+			FROM IrradianciasDias \
+			WHERE idLocalidad = ' + idLocalidad;
+
+		IrradianciasDias.query(query, function(err, rows) { 
+			res.json(rows);
+		});
+	}
 };
 
