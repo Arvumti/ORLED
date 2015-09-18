01faff47321f79a407a14eb41b8480829caf846f
MoarDante <alejandr0_gm@outlook.com>
2015-09-18 11:51:36 -0600

    cambios

--- assets/templates/views/orled.js
+++ assets/templates/views/orled.js
@@ -1,17 +1,27 @@
 define(['/js/base/viewsBase.js'], function (viewsBase) {
 	var ViOrled = Backbone.View.extend({
 		el: '#orled',
-		events: {			
+		events: {
+			'change [data-field="idLongitudTuberia"]': 'change_idLongitudTuberia',			
 		},
 		initialize: function() {
 			var that = this;			
-			this.fks = {				
+			this.fks = {
+				idPais: {
+					url: 'paises',
+					filters: [{filter:'nombre'}],
+				},
+				idEstado: {
+					url: 'estados',
+					filters: [{filter:'nombre'}],
+				},
 			};						
-
+			var tyas = this.$el.find('.tya');
+			viewsBase.popAbc.prototype.linkFks.call(this, tyas, this.fks);
+			debugger
 			this.gvBombas = this.$el.find('.gvBombas');
 			this.tmp_bombas = Handlebars.compile(this.$el.find('.tmp_bombas').html());
-			that.bombas();			
-
+			that.bombas();
 		},
 		/*------------------------- Base -----------------------------*/
 		render: function() {
@@ -19,6 +29,14 @@ define(['/js/base/viewsBase.js'], function (viewsBase) {
 		},
 		close: function() {
 			viewsBase.abc.prototype.close.call(this);
+		},
+		change_idLongitudTuberia: function(e) {
+			var valor = $(e.currentTarget).prop("checked");
+			debugger
+			if(valor)
+				this.documentosEntregados.find('[data-field="longitudTuberia"]').prop('disabled', true);
+			else
+				this.documentosEntregados.find('[data-field="longitudTuberia"]').prop('disabled', false);
 		},
 		bombas: function() {
 			app.ut.request({url:'/bombas',done:done});
